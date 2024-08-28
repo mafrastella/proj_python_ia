2a36cd56484d1927c181cbc35dd0d04bd5eb4162
Stella e Sabina <ribeirostellaribeiro@gmail.com>
2024-08-28 18:56:35 -0300

    laço de repetição

---
+++ aula03/para2.py
@@ -0,0 +1,20 @@
+alunos = [
+    "Stella",
+    "Sabina",
+    "Kaio",
+    "Richard",
+    "Bruno",
+    "Taylor",
+    "Travis",
+    "Lucas",
+    "Olivia",
+    "Julia"
+]
+alunos.sort()
+alunos.append("Wesley")
+alunos.reverse()
+y = 1
+print(y)
+for x in alunos:
+    print(y,"-",x)
+    y = y+1
