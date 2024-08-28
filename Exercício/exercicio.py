38d53265324dafe8914277948306af68d06165ef
Stella e Sabina <ribeirostellaribeiro@gmail.com>
2024-08-28 20:09:24 -0300

    laço de repetição

---
+++ Exercício/exercicio.py
@@ -0,0 +1,18 @@
+frutas = [
+    "morango",
+    "laranja",
+    "manga",
+    "uva",
+    "abacaxi",
+    "pêra",
+    "melância",
+    "melão",
+    "kiwi",
+    "jabuticaba"
+]
+frutas.sort()
+y = 1
+print(y)
+for x in frutas:
+    print(y,"-",x)
+    y = y+1
