d43fe6025692ede19df85f06873f5d4c51ed2bbc
Stella e Sabina <ribeirostellaribeiro@gmail.com>
2024-09-04 20:59:50 -0300

    metodos

---
+++ alua 04/Main.py
@@ -0,0 +1,23 @@
+from Calculadora import *
+from Lambda import *
+
+class Main:
+  x=int(input("escolha a sua operação:\n1 - adição\n2 - subtração\n3 - multiplicação\n4 - divisão\nescolha: "))
+  if (x==1):
+    a=int(input("digite o n1: "))
+    b=int(input("digite o n2: "))
+    print(Lambda.adi(a,b))
+  if(x==2):
+    a=int(input("digite o n1: "))
+    b=int(input("digite o n2: "))
+    print(Lambda.sub(a,b))
+  if(x==3):
+    a=int(input("digite o n1 :"))
+    b=int(input("digite o n2 :"))
+    print(Lambda.div(a,b))
+  if(x==4):
+   a=int(input("digite o n1 :"))
+   b=int(input("digite o n2 :"))
+   print(Lambda.mul(a,b))
+
+     
